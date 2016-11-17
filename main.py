46692e5201c0f752f424fda4cf10f71fdf61e035
jsyishan <404678173@qq.com>
2016-11-17 13:04:16 +0800

    use Singleton pattern to save the objects instantiated from the class 'PlayerInfo' also dealt with some data from the wows server(json)

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import json
 __author__ = 'Eugen'
 
 
-class CrawlerOfWOWS(object):
-    def __init__(self):
-        self.data = {}
+class PlayerInfo(object):
+    def __init__(self, name, zone):
+        self.data = {'player': name, 'zone': zone}
         self.url = 'http://rank.kongzhong.com/wows/index.html'
         self.url_get_user_info = 'http://rank.kongzhong.com/Data/action/WowsAction/getLogin'
         self.usr_get_data_info = 'http://rank.kongzhong.com/Data/action/WowsAction/getShipInfo'
@@ -18,7 +18,8 @@ class CrawlerOfWOWS(object):
                                          ' Chrome/54.0.2840.99 '
                                          'Safari/537.36')
 
-        print('Step 1... init')
+        print('Step 1 ... init')
+        self.get_info()
 
     def get_user_info(self):
         req_url = self.url_get_user_info + '?name=' + self.data['player'] + '&zone=' + self.data['zone']
@@ -55,12 +56,19 @@ class CrawlerOfWOWS(object):
 
             calc_info('battles')
             calc_info('teambattles')
+            calc_info('wins')
+            calc_info('teamwins')
+            calc_info('damage')
+
             self.data['singlebattles'] = self.data['battles'] - self.data['teambattles']
+            self.data['singlewins'] = self.data['wins'] - self.data['teamwins']
+            self.data['winrate'] = self.data['wins'] / self.data['battles']
+            self.data['teamwinrate'] = self.data['teamwins'] / self.data['teambattles']
+            self.data['singlewinrate'] = self.data['singlewins'] / self.data['singlebattles']
 
-    def input_player(self, name, zone):
-        self.data['player'] = name
-        self.data['zone'] = zone
 
+    def get_info(self):
+        print('Trying connecting to the server...')
         self.get_user_info()
         self.get_data_info()
 
@@ -74,18 +82,28 @@ class CrawlerOfWOWS(object):
         #     print('Status:', f.status, f.reason)
         #     self.soup_match()
 
-    # def soup_match(self):
-    #     soup = BeautifulSoup(self.data['data'], 'html.parser', from_encoding='utf-8')
-    #     content = soup.find('div', id='total').findAll('span', {'class': 'value'})
-    #     for i in content:
-    #         print(i.string)
+
+# def soup_match(self):
+#     soup = BeautifulSoup(self.data['data'], 'html.parser', from_encoding='utf-8')
+#     content = soup.find('div', id='total').findAll('span', {'class': 'value'})
+#     for i in content:
+#         print(i.string)
+
+def get_user():
+    # name = parse.quote(input('玩家昵称：'))
+    name = parse.quote('pnq-Ar196-欧根亲王')
+    zone = input('分区(默认南区)： ')
+    if not zone == '':
+        if not zone == 'south' and not zone == 'north':
+            print('请输入正确的分区!')
+            return
+        players.append(PlayerInfo(name, zone))
+    else:
+        players.append(PlayerInfo(name, 'south'))
 
 
 if __name__ == '__main__':
-    crawler = CrawlerOfWOWS()
-    # name = input('玩家昵称:')
-    input_name = 'pnq-Ar196-欧根亲王'
-    url_name = parse.quote(input_name)
-    # zone = input('分区：')
-    input_zone = 'south'
-    crawler.input_player(url_name, input_zone)
+    players = []
+    get_user()
+
+
